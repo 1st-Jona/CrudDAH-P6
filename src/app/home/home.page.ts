5ee60561e337c4ce4ebc0a1b7e6c9e04c1c6fb8d
1st-Jona <joislopezsa@ittepic.edu.mx>
2020-12-28 12:59:19 -0700

    Part three and four from CRUD

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { EstudianteService } from "../services/estudiante.service";
 import { Estudiante } from '../models/estudiante';
+import { Router, NavigationExtras } from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,7 @@ import { Estudiante } from '../models/estudiante';
 export class HomePage {
   public students:Estudiante[];
 
-  constructor(private service:EstudianteService) {
+  constructor(private service:EstudianteService,private router:Router) {
     this.service.getStudents().subscribe(data=>{
       this.students=data.map(e=>{
         return{
@@ -23,6 +24,20 @@ export class HomePage {
         }as Estudiante
       })
     })
+  }
+
+  update(student:Estudiante,active:boolean){
+    student.active=active;
+    this.service.updateStudent(student,student.id);
+  }
+
+  detail(student:Estudiante){
+    let navext:NavigationExtras ={
+      queryParams:{
+        special:JSON.stringify(student)
+      }
+    };
+    this.router.navigate(['/detail'],navext);
   }
  
 }
